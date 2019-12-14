9c7a27e7b7b7c0ae0abe5c5a68083c2790f33154
Lucas Martin-Chabalier <lucas.martinchabalier@gmail.com>
2019-12-14 22:38:36 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# My personal website!
-
-Access at [lucactusss.github.io](lucactusss.github.io)
---
+++ precache-manifest.2461e3b5be9ee110a43bb84b70e23fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170ba53d27f7e54cf4cdd1a07c5e4860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8633a140aae4921633b8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f9659eac333b6deee519",
+    "url": "/static/js/2.07c0aa6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07c0aa6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8633a140aae4921633b8",
+    "url": "/static/js/main.007d6d8c.chunk.js"
+  },
+  {
+    "revision": "fca0ee15369702b4b83e",
+    "url": "/static/js/runtime-main.d5a45d37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
